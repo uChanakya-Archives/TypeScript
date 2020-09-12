34f7724ac230e28fe0b43b61ebc8457257f447ec
U-C-S <ucs3721@outlook.com>
2020-09-12 11:50:26 +0530

    ts 5.1

--- private/unit-convert.ts
+++ private/unit-convert.ts
@@ -1,13 +1,74 @@
-// 1 x is equal to con_factor y (x is a unit; y is respective SI/base unit) 
-//Ex: 1 mm is equal to 1/1000 m
-class unit_factor {
-  unit: string[];
-  con_factor: number;
-  constructor(x: string[], y: number) {
-    this.unit= x;
-    this.con_factor= y
+function unit_convert(usertyped: string){
+  const ucs_data = usertyped.split(' ');
+
+  if(ucs_data.length == 5){ TheConverter(Number(ucs_data[1]) , ucs_data[2] , ucs_data[4]); }
+
+  else if(ucs_data.length == 4){
+    let uVal = parseFloat(ucs_data[1]);
+    let uValLeng = String(uVal).replace('.', ' ').length
+    let uUnit = ucs_data[1].slice(uValLeng);
+    TheConverter(uVal , uUnit , ucs_data[3]);
+  }
+
+  else{
+    talk_div_boct('x_x')
   }
 }
+
+function TheConverter(x: number , a: string , b: string) {
+  let from_unit = getUnit(a);
+  let to_unit = getUnit(b);
+
+  if(from_unit.Available && to_unit.Available){
+    if(from_unit.Category == to_unit.Category){
+      if(from_unit.con_factor && to_unit.con_factor){
+        let result = x * (from_unit.con_factor / to_unit.con_factor);
+        return talk_div_boct(`${result} ${b}`);
+      }
+      else if(from_unit.con_trnsTo && to_unit.con_trnsFro){
+        let SIval = from_unit.con_trnsTo(x);
+        let result = to_unit.con_trnsFro(SIval);
+        return talk_div_boct(`${result} ${b}`);
+      }
+    }
+    else{
+      return replyRandom(['-_-','Conversions do not work that way']);
+    }
+  }
+
+  else{ return talk_div_boct('Something is very Wrong here.') }
+}
+
+function getUnit(u: any) {
+  const Categories = [Lengths,Areas,Mass,Temperatures,Volume];
+  var unit_details,
+      i: number = 0;
+
+  while(i < Categories.length && (!unit_details)){
+    const CategoryType = Categories[i];
+    let obj = Object.keys(CategoryType);
+    obj.forEach((j) => {
+      if( CategoryType[j].unit.includes(u) ) {
+        let pre_unit_detail = {
+          Available: true,
+          UserUnit: u,
+          Category: i,
+        }
+        unit_details = Object.assign(pre_unit_detail,CategoryType[j]);
+      }
+    })
+    i++
+  }
+
+  if(!unit_details){
+    unit_details={Available: false}
+  }
+
+  return unit_details;
+}
+
+// 1 x is equal to con_factor y (x is a unit; y is respective SI/base unit) 
+//Ex: 1 mm is equal to 1/1000 m
 const Lengths = {
   m: {
     unit: ['m', 'meter', 'meters', 'metre'],
